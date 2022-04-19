2947d7933cd3302ec190ba984fadfef25c9c522d
halykon <sicstar21@gmail.com>
2022-04-19 16:22:47 +0200

    Initial

---
+++ stormborn.lua
@@ -0,0 +1,52 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----- 
+-- BACKGROUND 
+-- ----- 
+
+-- ----- 
+--  BORDER 
+-- ---- 
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|cFF3979CDStormborm|r - bam", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - bam.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - bam2", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - bam2.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - beyblade", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - beyblade.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - boom", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - boom.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - boomboom", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - boomboom.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - boomjunge", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - boomjunge.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - boompow", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - boompow.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - boomvollmond", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - boomvollmond.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - cloudburst", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - cloudburst.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - combobreaker", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - combobreaker.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - cooldown", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - cooldown.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - dbzkick", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - dbzkick.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - dbzpunch", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - dbzpunch.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - explosion", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - explosion.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - fairytail", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - fairytail.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - falconpunch", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - falconpunch.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - fatality", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - fatality.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - finishhim", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - finishhim.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - gasgasgas", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - gasgasgas.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - huaboom", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - huaboom.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - huah", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - huah.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - lappen", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - lappen.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - lifebloom", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - lifebloom.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - markus", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - markus.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - ora", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - ora.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - oraoraora", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - oraoraora.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - pow", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - pow.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - starplatinum", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - starplatinum.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - stereosaiyan", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - stereosaiyan.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - stereosaiyan20s", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - stereosaiyan20s.ogg]]) 
+LSM:Register("sound", "|cFF3979CDStormborm|r - wuuu", [[Interface\Addons\SharedMedia_stormborn\sound\Stormborm - wuuu.ogg]]) 
+
+-- -----
+--   STATUSBAR
+-- -----
